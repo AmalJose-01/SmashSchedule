9a30755b660ef24e3d22d331ee97da590df0ee1c
Amal Jose <amaljvv@gmail.com>
2026-01-07 17:19:09 +1100

    Email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,5 +1,21 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+
 require("dotenv").config();
+
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+});
+
+
 
 const sendEmail = async ({ to, subject, html }) => {
  
@@ -17,16 +33,29 @@ try {
 // });
 
 
-  const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-      },
-    });
+  // const transporter = nodemailer.createTransport({
+  //     service: "gmail",
+  //     auth: {
+  //       type: "OAuth2",
+  //       user: process.env.EMAIL_USER,
+  //       clientId: process.env.GOOGLE_CLIENT_ID,
+  //       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  //       refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+  //     },
+  //   });
+  const accessToken = await oauth2Client.getAccessToken();
+  
+  const transporter = nodemailer.createTransport({    
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL_USER,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      accessToken: accessToken.token,
+    },
+  });
 
 
 console.log("Mail service =====================");
