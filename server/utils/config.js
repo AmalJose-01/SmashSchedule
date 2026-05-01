14302ee4b4073fb46387cbac8bfa194794e7d37e
Amal Jose <amal.jose@gmail.com>
2026-05-02 09:57:06 +1000

    conf

--- server/utils/config.js
+++ server/utils/config.js
@@ -4,13 +4,15 @@ const branch = process.env.VERCEL_GIT_COMMIT_REF || "local";
 let BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
 if (env === "production") {
-  BASE_URL = "https://api-prod.onrender.com";
-} else if (env === "preview") {
+  BASE_URL = "https://api-prod-orvq.onrender.com";
+}
+
+if (env === "preview") {
   if (branch === "qa") {
-    BASE_URL = "https://api-qa.onrender.com";
+    BASE_URL = "https://main-qa.onrender.com";
   } else if (branch === "qanext") {
-    BASE_URL = "https://api-qanext.onrender.com";
+    BASE_URL = "https://qa-next.onrender.com";
   }
 }
 
-export default BASE_URL;
+export default BASE_URL;`   `
