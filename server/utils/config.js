5cf8e48832d4d35eafc27465ebff4e669ec9d9c4
Amal Jose <amal.jose@gmail.com>
2026-05-03 12:19:19 +1000

    backend

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,19 +1,17 @@
 // src/config/api.js
 
-const env = import.meta.env.MODE; // "development" or "production"
+const env = import.meta.env.MODE; // development | production
 const branch = import.meta.env.VITE_BRANCH || "local";
 
 let BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
-if (env === "production") {
-  BASE_URL = "https://api-prod-orvq.onrender.com";
-}
 
 if (env === "production") {
   if (branch === "qa") {
     BASE_URL = "https://main-qa.onrender.com";
   } else if (branch === "qanext") {
     BASE_URL = "https://qa-next.onrender.com";
+  } else {
+    BASE_URL = "https://api-prod-orvq.onrender.com";
   }
 }
 
